a657c8a78027b3ebc61bd70583dd168d781ba664
lopeselio <lopeselio@gmail.com>
2020-08-18 11:51:53 +0530

    password hash if modified

--- server/models/user.js
+++ server/models/user.js
@@ -4,11 +4,11 @@ const SALT_I = 10
 
 const userSchema = mongoose.Schema({
   email: {
-    type: String, 
-    required: true, 
+    type: String,
+    required: true,
     trim: true,
     unique: 1
-  }, 
+  },
   password: {
     type: String,
     required: true,
@@ -16,12 +16,12 @@ const userSchema = mongoose.Schema({
   },
   name: {
     type: String,
-    required: true, 
+    required: true,
     maxlength: 100
   },
   lastname: {
     type: String,
-    required: true, 
+    required: true,
     maxlength: 100
   },
   cart: {
@@ -30,8 +30,8 @@ const userSchema = mongoose.Schema({
   },
   history: {
     type: Array,
-    default: [] 
-  }, 
+    default: []
+  },
   role: {
     type: Number,
     default: 0
@@ -39,20 +39,25 @@ const userSchema = mongoose.Schema({
   token: {
     type: String
   }
-userSchema.pre('save', function(next){
-  var user = this;
-  bcrypt.genSalt(SALT_I, function(err, salt){
-    if(err) return next(err)
+})
 
-    bcrypt.hash(user.password, salt, function(err,hash){
-      if(err) return next(err)
-      user.password = hash
-      next()
+userSchema.pre('save', function (next) {
+  var user = this
+  if (user.isModified('password')) {
+    bcrypt.genSalt(SALT_I, function (err, salt) {
+      if (err) return next(err)
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err)
+        user.password = hash
+        next()
+      })
     })
-  })
-})
+  } else {
+    next()
+  }
 })
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
