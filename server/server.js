cf20fa8ffd397bc3bdfd1573c44bc505a87bcb64
lopeselio <lopeselio@gmail.com>
2020-08-18 10:49:04 +0530

    server setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
+
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.DATABASE)
-app.use(bodyParser.urlencoded())
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
+
+const { User } = require('./models/user')
+// ==============================
+//            USERS
+// ==============================
+
+app.post('/api/usres/register', (req, res) => {
+  res.status(200)
+})
+
+app.post('/api/users/register', (req, res) => {
+  res.status(200)
+})
+
 const port = process.env.PORT || 3002
 app.listen(port, () => {
   console.log(`Server Running at ${port}`)
