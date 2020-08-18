efd1556ccbb39f724a40f5d4c58cc25c0ee6f350
lopeselio <lopeselio@gmail.com>
2020-08-18 13:49:33 +0530

    jwt tokenisation

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const SALT_I = 10
 
 const userSchema = mongoose.Schema({
@@ -63,6 +64,12 @@ userSchema.methods.comparePassword = function (candidatePassword, cb) {
     if (err) return cb(err)
     cb(null, isMatch)
   })
+}
+
+userSchema.methods.generateToken = function () {
+  var user = this;
+  var token = jwt.sign()
+  // user.id + password
 }
 
 const User = mongoose.model('User', userSchema)
